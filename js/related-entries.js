0c7b8e38335ce4df7bcd485d3e34dc688b810f26
Angel Castillo <32829371+ACast59@users.noreply.github.com>
2018-12-14 11:49:49 -0400

    New Template

--- js/related-entries.js
+++ js/related-entries.js
@@ -1,44 +1,42 @@
-$("#related-posts").each(function() {
-  var b = $(this).text();
-  $.ajax({
-    url: "/feeds/posts/default/-/" + b + "?alt=json-in-script&max-results=3",
-    type: 'get',
-    dataType: "jsonp",
-    success: function(e) {
-      var u = "";
-      var h = '<div class="related-posts">';
-      for (var i = 0; i < e.feed.entry.length; i++) {
-        for (var j = 0; j < e.feed.entry[i].link.length; j++) {
-          if (e.feed.entry[i].link[j].rel == "alternate") {
-            u = e.feed.entry[i].link[j].href;
-            break
-          }
-        }
-        var g = e.feed.entry[i].title.$t;
-        var d = e.feed.entry[i].published.$t,
-        v = d.substring(0, 4),
-        w = d.substring(5, 7),
-        f = d.substring(8, 10),
-        r = MONTH_FORMAT[parseInt(w, 10)] + ' ' + f + ', ' + v;
-        var c = e.feed.entry[i].content.$t;
-        var $c = $('<div>').html(c);
-        if (c.indexOf("<img") > -1) {
-          var q = $c.find('img:first').attr('src').replace('s72-c', 's1600');
-          var k = q
-        } else {
-          var k = NO_IMAGE
-        }
-        h += '<div class="col-12 col-sm-4"><div class="card card-widget"><div class="card-img"><a href="' + u + '"><img class="related-img" src="' + k + '" alt="' + g + '"/></a></div><div class="card-block"><h4 class="card-title"><a href="' + u + '">' + g + '</a></h4><div class="card-meta"><span><i class="fa fa-clock-o"></i> ' + r + '</span></div></div></div></div>'
+function related_results_labels(e) {
+  for (var l = 0; l < e.feed.entry.length; l++) {
+    var t = e.feed.entry[l];
+    relatedTitles[relatedTitlesNum] = t.title.$t;
+    var r = e.feed.entry[l].media$thumbnail.url,
+      a = r.replace("/s72-c/", "/w320-h230-c/");
+    if (e.feed.entry[l].media$thumbnail) var r = e.feed.entry[l].media$thumbnail.url,
+      a = r.replace("/s72-c/", "/w320-h230-c/");
+    else if (null != e.feed.entry[l].content.$t.match(/src=(.+?[\.jpg|\.gif|\.png]")/)) var a = e.feed.entry[l].content.$t.match(/src=(.+?[\.jpg|\.gif|\.png]")/)[1];
+    else var a = "http://1.bp.blogspot.com/-a-3WZRtj7pw/VoxaVk-cPMI/AAAAAAAABMo/ivQ1HVw0ZME/s250-Ic42/no-thumbnail.png";
+    relatedImage[relatedTitlesNum] = a;
+    for (var n = 0; n < t.link.length; n++)
+      if ("alternate" == t.link[n].rel) {
+        relatedUrls[relatedTitlesNum] = t.link[n].href, relatedTitlesNum++;
+        break
       }
-      h += '</div>';
-      $("#related-posts").html(h);
-      $('.related-img').each(function() {
-        $(this).attr('style', function(i, src) {
-          return src.replace('/default.jpg', '/hqdefault.jpg')
-        }).attr('style', function(i, src) {
-          return src.replace('s72-c', 's1600')
-        })
-      })
-    }
-  })
-});
+  }
+}
+
+function removeRelatedDuplicates() {
+  for (var e = new Array(0), l = new Array(0), t = new Array(0), r = 0; r < relatedUrls.length; r++) contains(e, relatedUrls[r]) || (e.length += 1, e[e.length - 1] = relatedUrls[r], l.length += 1, l[l.length - 1] = relatedTitles[r], t.length += 1, t[t.length - 1] = relatedImage[r]);
+  relatedTitles = l, relatedUrls = e, relatedImage = t
+}
+
+function contains(e, l) {
+  for (var t = 0; t < e.length; t++)
+    if (e[t] == l) return !0;
+  return !1
+}
+
+function printRelatedLabels(e) {
+  for (var l = 0; l < relatedUrls.length; l++) relatedUrls[l] == e && (relatedUrls.splice(l, 1), relatedTitles.splice(l, 1), relatedImage.splice(l, 1));
+  var t = Math.floor((relatedTitles.length - 1) * Math.random()),
+  l = 0;
+  for (relatedTitles.length > 1; l < relatedTitles.length && 20 > l && l < maxposts;) l % 2 == 1 ? document.getElementById("related_items").innerHTML += "<div class='col-12 col-sm-4'><div class='card card-widget'><div class='card-img'><a href='" + relatedUrls[t] + "'><img src='" + relatedImage[t] + "' alt=''/>" + relatedTitles[t] + "</a></div><div class='card-block'><h4 class='card-title'><a href='" + relatedUrls[t] + "'>" + relatedTitles[t] + "</a></h4></div></div></div>" : document.getElementById("related_items").innerHTML += "<div class='col-12 col-sm-4'><div class='card card-widget'><a href='" + relatedUrls[t] + "'><img src='" + relatedImage[t] + "' alt='" + relatedTitles[t] + "'/>" + relatedTitles[t] + "</a></div></div></div>", t < relatedTitles.length - 1 ? t++ : t = 0, l++;
+  relatedUrls.splice(0, relatedUrls.length), relatedTitles.splice(0, relatedTitles.length), relatedImage.splice(0, relatedImage.length)
+}
+
+var relatedTitles = new Array,
+var relatedImage = new Array,
+var relatedTitlesNum = 0,
+var relatedUrls = new Array;
